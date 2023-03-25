30af8b49894d901963697ca2d0c19073f193935b
S <s@Mac-mini.local>
2023-03-25 21:35:11 +0900

    調整

--- src/App.vue
+++ src/App.vue
@@ -1,89 +1,90 @@
 <template>
-  <div class="wrap">
-    <h1>TODOリスト</h1>
-    <p class="explanation">小さいリストはドラッグアンドドロップで移動できます<br>ページを閉じても内容が1時間残ります</p>
-    <ul class="todo-list">
-      <li class="todo-list__item" v-for="(todo, index) in parentTodos" :key="todo.key">
-        <div class="todo-list__head">
-          <input class="todo-list__input parent-input" type="text" v-model="parentTodos[index].text" @input="todoSave">
-          <div class="todo-list__btn-wrap">
-            <button class="todo-list__btn add-btn" @click="addParent"></button>
-            <button class="todo-list__btn del-btn" @click="deleteTodo(index)"></button>
-          </div>
-        </div>
-        <childTodo :index=index></childTodo>
-      </li>
-    </ul>
-  </div>
+	<div class="wrap">
+		<h1>TODOリスト</h1>
+		<p class="explanation">小さいリストはドラッグアンドドロップで移動できます<br />ページを閉じても内容が1時間残ります</p>
+		<ul class="todo-list">
+			<li class="todo-list__item" v-for="(todo, index) in parentTodos" :key="todo.key">
+				<div class="todo-list__head">
+					<input class="todo-list__input parent-input" type="text" v-model="parentTodos[index].text" @input="todoSave" />
+					<div class="todo-list__btn-wrap">
+						<button class="todo-list__btn add-btn" @click="addParent"></button>
+						<button class="todo-list__btn del-btn" @click="deleteTodo(index)"></button>
+					</div>
+				</div>
+				<childTodo :index="index"></childTodo>
+			</li>
+		</ul>
+	</div>
 </template>
 
 <script>
-  import { ref, onMounted } from 'vue'
-  import childTodo from '@/components/childTodo.vue'
+import { ref, onMounted } from 'vue';
+import childTodo from '@/components/childTodo.vue';
 
-  export default {
-    components: {
-      childTodo,
-    },
-    setup() {
-      const parentTodos = ref([])
-      const parentCount = ref(0)
+export default {
+	components: {
+		childTodo,
+	},
+	setup() {
+		const parentTodos = ref([]);
+		const parentCount = ref(0);
 
-      const cookies = ref(document.cookie)
-      const cookiesArray = ref(cookies.value.split(';'))
-      const cookieFlg = ref(false)
+		const cookies = ref(document.cookie);
+		const cookiesArray = ref(cookies.value.split(';'));
+		const cookieFlg = ref(false);
 
-      const cookieTodo = () => {
-        for(let cookie of cookiesArray.value){
-          let cookieName = cookie.trim().substr(0, 5)
-          if( cookieName == 'todo='){
-            let json = cookie.trim().slice(5)
-            parentTodos.value = JSON.parse(json)
-            cookieFlg.value = true
-            break
+		const cookieTodo = () => {
+			for (let cookie of cookiesArray.value) {
+				let cookieName = cookie.trim().substr(0, 5);
+				if (cookieName == 'todo=') {
+					let json = cookie.trim().slice(5);
+					parentTodos.value = JSON.parse(json);
+          if(json !== '[]') {
+            cookieFlg.value = true;
           }
-        }
-        if(cookieFlg.value) {
-          parentCount.value = parentTodos.value.length
-        }
-      }
-      cookieTodo()
+					break;
+				}
+			}
+			if (cookieFlg.value) {
+				parentCount.value = parentTodos.value.length;
+			}
+		};
+		cookieTodo();
+		const addParent = () => {
+			parentTodos.value.push({ text: [], key: [] });
+			parentTodos.value[parentCount.value].text.push('');
+			parentTodos.value[parentCount.value].key.push(new Date().getTime().toString());
+			parentCount.value++;
+			todoSave();
+		};
+		const deleteTodo = (index) => {
+			if (parentCount.value > 0) {
+				parentTodos.value.splice(index, 1);
 
-      onMounted(() => {
-        if(!cookieFlg.value) {
-          addParent()
-        } 
-      })
-      const addParent = () => {
-        parentTodos.value.push({ text: [], key: [] })
-        parentTodos.value[parentCount.value].text.push('')
-        parentTodos.value[parentCount.value].key.push(new Date().getTime().toString())
-        parentCount.value++
-        todoSave()
-      }
-      const deleteTodo = (index) => {
-        if(parentCount.value > 0) {
-          parentTodos.value.splice(index, 1)
-          
-          parentCount.value--
-          todoSave()
-          document.cookie = 'todoChild' + parentCount.value + '=; max-age=0;'
-        }
-      }
-      const todoSave = () => {
-        document.cookie = 'todo =' + JSON.stringify(parentTodos.value) + '; max-age=3600;'
-      }
-      return {
-        parentTodos,
-        parentCount,
-        cookies,
-        cookiesArray,
-        cookieFlg,
-        addParent,
-        deleteTodo,
-        cookieTodo,
-        todoSave, 
-      }
-    }
-  }
+				parentCount.value--;
+				todoSave();
+				document.cookie = 'todoChild' + parentCount.value + '=; max-age=0;';
+			}
+		};
+		const todoSave = () => {
+			document.cookie = 'todo =' + JSON.stringify(parentTodos.value) + '; max-age=3600;';
+		};
+    onMounted(() => {
+			if (!cookieFlg.value) {
+				addParent();
+			}
+		});
+		return {
+			parentTodos,
+			parentCount,
+			cookies,
+			cookiesArray,
+			cookieFlg,
+			addParent,
+			deleteTodo,
+			cookieTodo,
+			todoSave,
+		};
+	},
+};
 </script>
--- src/components/childTodo.vue
+++ src/components/childTodo.vue
@@ -2,9 +2,9 @@
   <div class="child-todo">
     <button class="child-add-btn" @click="addChild()">リスト追加</button>
     <draggable class="child-list" v-model="childTodos" item-key="todo" group="todo">
-      <template #item="{index}">
+      <template #item="{ index }">
         <div class="child-list__item">
-          <input class="child-list__input child-input" type="text" v-model="childTodos[index]" @input="inputTodo">
+          <input class="child-list__input child-input" type="text" v-model="childTodos[index]" @input="inputTodo" />
           <button class="child-list__btn del-btn" @click="deleteTodoChild(index)"></button>
         </div>
       </template>
@@ -13,73 +13,73 @@
 </template>
 
 <script>
-  import { ref, onMounted, } from 'vue'
-  import draggable from 'vuedraggable'
+import { ref, onMounted } from "vue";
+import draggable from "vuedraggable";
 
-  export default {
-    components: {
-      draggable
-    },
-    props: ['index'],
-    setup(props) {
-      const childTodos = ref([])
-      
-      const todoIndex = ref(props.index)
+export default {
+  components: {
+    draggable,
+  },
+  props: ["index"],
+  setup(props) {
+    const childTodos = ref([]);
 
-      const cookies = ref(document.cookie)
-      const cookiesArray = ref(cookies.value.split(';'))
-      const cookieFlg = ref(false)
+    const todoIndex = ref(props.index);
 
-      const cookieTodoChild = () => {
-        for(let cookie of cookiesArray.value){
-          let cookieName = 'todoChild' + todoIndex.value + '='
-          if(cookie.indexOf(cookieName) != -1){
-            let json = cookie.trim().slice(cookieName.length)
-            childTodos.value = JSON.parse(json)
-            cookieFlg.value = true
-            break
-          }
+    const cookies = ref(document.cookie);
+    const cookiesArray = ref(cookies.value.split(";"));
+    const cookieFlg = ref(false);
+
+    const cookieTodoChild = () => {
+      for (let cookie of cookiesArray.value) {
+        let cookieName = "todoChild" + todoIndex.value + "=";
+        if (cookie.indexOf(cookieName) != -1) {
+          let json = cookie.trim().slice(cookieName.length);
+          childTodos.value = JSON.parse(json);
+          cookieFlg.value = true;
+          break;
         }
       }
-      cookieTodoChild()
-      onMounted(() => {
-        if(!cookieFlg.value) {
-          addChild(todoIndex.value)
-        } 
-      })
-      const addChild = () => {
-        childTodos.value.push('')
-        todoSave()
+    };
+    cookieTodoChild();
+    const addChild = () => {
+      childTodos.value.push("");
+      todoSave();
+    };
+    const deleteTodoChild = (index) => {
+      if (childTodos.value.length > 1) {
+        console.log(childTodos.value.length);
+        childTodos.value.splice(index, 1);
+        todoSave();
       }
-      const deleteTodoChild = (index) => {
-        if(childTodos.value.length > 1) {
-          console.log(childTodos.value.length)
-          childTodos.value.splice(index, 1)
-          todoSave()
-        }
+    };
+    const todoSave = () => {
+      document.cookie = "todoChild" + todoIndex.value + "=" + JSON.stringify(childTodos.value) + "; max-age=3600;";
+    };
+    const inputTodo = () => {
+      todoSave();
+    };
+    onMounted(() => {
+      if (!cookieFlg.value) {
+        addChild(todoIndex.value);
       }
-      const todoSave = () => {
-        document.cookie = 'todoChild' + todoIndex.value + '=' + JSON.stringify(childTodos.value) + '; max-age=3600;'
-      }
-      const inputTodo = () => {
-        todoSave()
-      }
-      return {
-        childTodos,
-        cookies,
-        cookiesArray,
-        cookieFlg,
-        todoIndex,
-        inputTodo,
-        addChild,
-        deleteTodoChild,
-        cookieTodoChild,
-        todoSave,
-      }
-    }
-  }
+    });
+    return {
+      childTodos,
+      cookies,
+      cookiesArray,
+      cookieFlg,
+      todoIndex,
+      inputTodo,
+      addChild,
+      deleteTodoChild,
+      cookieTodoChild,
+      todoSave,
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-@import '@/assets/scss/childTodo.scss';
+@import "@/assets/scss/childTodo.scss";
 </style>
